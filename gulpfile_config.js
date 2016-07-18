8a70600928086ca277278ed504f2318f6889ca5c
caro_huang <caro_huang@hiiir.com>
2016-07-18 14:00:11 +0800

    add for 相容性

--- gulpfile_config.js
+++ gulpfile_config.js
@@ -1,9 +1,13 @@
 module.exports = (function () {
   var self = {};
   // 要優先自動掃描並加到 html js / css tag 的檔案
+  // 相容性程式優先載入 https://github.com/es-shims/es5-shim
   self.headArr = [
     '_compatibility/es5-shim.min.js',
     '_compatibility/es5-sham.min.js',
+    '_compatibility/json3.min.js',
+    '_compatibility/es6-shim.min.js',
+    '_compatibility/es6-sham.min.js',
     '_compatibility/html5.js',
     '_bower/lodash.js',
     '_bower/**/*.*',
