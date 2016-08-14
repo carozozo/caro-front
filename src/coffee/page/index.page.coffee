2a81013101b78b0f256267c3a5a688818e25e4fe
caro_huang <caro_huang@hiiir.com>
2016-08-14 12:24:15 +0800

    update intro

--- src/coffee/page/index.page.coffee
+++ src/coffee/page/index.page.coffee
@@ -1,9 +1,19 @@
 cf.router.regPage 'index', (cf, $page) ->
   window = cf.require('window')
   caro = cf.require('caro')
+  $ = cf.require('$')
   _tracking = cf.tracking
 
   _tracking.page('index')
+
+  $page.dom('.title').each (i, $title) ->
+    $title = $($title).dom()
+    $subContents = $title.parent().dom('.subContent')
+    $title.onClick ->
+      $subContents.fadeToggle()
+      return
+    return
+
   $page
 
 cf.regDocReady ->
--- src/js/page/index.page.js
+++ src/js/page/index.page.js
@@ -1,9 +1,18 @@
 cf.router.regPage('index', function(cf, $page) {
-  var _tracking, caro, window;
+  var $, _tracking, caro, window;
   window = cf.require('window');
   caro = cf.require('caro');
+  $ = cf.require('$');
   _tracking = cf.tracking;
   _tracking.page('index');
+  $page.dom('.title').each(function(i, $title) {
+    var $subContents;
+    $title = $($title).dom();
+    $subContents = $title.parent().dom('.subContent');
+    $title.onClick(function() {
+      $subContents.fadeToggle();
+    });
+  });
   return $page;
 });
 
