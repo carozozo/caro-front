df513c5dfdf98152feabc4ce07513e630e175f06
caro_huang <caro_huang@hiiir.com>
2016-08-15 15:40:31 +0800

    remove plugin animate

--- src/coffee/module/animated.coffee
+++
@@ -1,5 +0,0 @@
-### Animate.css 擴充 http://daneden.github.io/animate.css ###
-cf.regModule 'cfAnimated', (className) ->
-  $self = @
-  $self.addClass('animated ' + className)
-  $self
--- src/js/module/animated.js
+++
@@ -1,8 +0,0 @@
-
-/* Animate.css 擴充 http://daneden.github.io/animate.css */
-cf.regModule('cfAnimated', function(className) {
-  var $self;
-  $self = this;
-  $self.addClass('animated ' + className);
-  return $self;
-});
